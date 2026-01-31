09fcdec6a9e4f80cab6fcb356a345e96920b2142
Joseph <jwhenry28@gmail.com>
2026-01-31 06:20:30 -0600

    initialize backend styling + various tweaks

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pray-with-us
+
+## backend development
+
+Two terminals:
+```bash
+$ npm run dev
+$ npm run dev:css
+```
+
+## mobile development
+
+```bash
+$ npx expo start -c
+# OR
+$ npm run start
+```
+
+Need Android emulator running with Expo app installed.
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from "express";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello, world!" });
+  res.render("index", { title: "My App" });
 });
 
 app.listen(port, () => {
