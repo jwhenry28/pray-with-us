1a10fea63df25c6f55c19176c7f6654a933be862
Joseph <jwhenry28@gmail.com>
2026-01-31 13:26:26 -0600

    init styling done

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+import NavBar from "./Navigation";
+
 const RootLayout = () => {
   return (
     <>
+      <NavBar />
       <main className="bg-pwu-primary min-h-screen pt-16">
         <Outlet />
       </main>
